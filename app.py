9e21993af5ef6b5fdde62d3f15aeae77ca16ed13
“Vansh <vanshgulati110@gmail.com>
2026-04-06 22:41:18 +0200

    change in subtraction function

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def addition(a, b, c):
     return a + b + c
 
-def subtraction(a, b, e):
-    return a - b - e
+def subtraction(a, b, d):
+    return a - b - d
 
 def division(a, b):
     if b != 0:
